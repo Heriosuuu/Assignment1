924526da9d4cd7e78a568478f0602c6ca7e2be92
Heriosuuu <147970062+Heriosuuu@users.noreply.github.com>
2023-12-01 00:23:48 +0800

    Assignment 1

--- Javascripts/top.js
+++ Javascripts/top.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+    /* Variable to get ID name from html*/
     var backToTopButton = document.getElementById("backToTopBtn");
 
+    /* Scroll function*/
     window.onscroll = function () {
+        /* Once user scroll over 20units, button will appear, if not button will be hidden*/
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             backToTopButton.style.display = "block";
         } else {
@@ -13,9 +16,11 @@ document.addEventListener("DOMContentLoaded", function () {
         scrollToTop();
     };
 
+    /* Scrolling to top function*/
     function scrollToTop() {
         window.scrollTo({
             top: 0,
+            /* Smooth scrolling behavior*/
             behavior: "smooth"
         });
     }
--- Javascripts/video.js
+++ Javascripts/video.js
@@ -1,14 +1,18 @@
 function videoPlay() {
 
+    /* Variables for video and button ID*/
     var video = document.getElementById('theBrave');
     var btn = document.getElementById('videoBtn');
 
-    console.log(btn);
+    /* Checks if video is playing*/
+
     if (video !== null) {
+      /* If video is paused, button will start the video and change text from "Play" to "Paused"*/
       if (video.paused) {
         video.play();
         btn.innerHTML = "Pause";
       } else {
+        /* Other way wrong from the above feature*/
         video.pause();
         btn.innerHTML = "Play";
       }

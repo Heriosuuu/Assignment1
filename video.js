cf6bbe2a6bce8710377f3cbf5d2d4e17a250fa18
Heriosuuu <s10262073@connect.np.edu.sg>
2023-11-30 13:51:30 +0800

    Assignment 1

--- video.js
+++ video.js
@@ -2,7 +2,8 @@ function videoPlay() {
 
     var video = document.getElementById('theBrave');
     var btn = document.getElementById('videoBtn');
-  
+
+    console.log(btn);
     if (video !== null) {
       if (video.paused) {
         video.play();
@@ -15,3 +16,25 @@ function videoPlay() {
       console.error("Video element not found!");
     }
   }
+
+
+
+  function audioPlay() {
+
+    var audio = document.getElementById('running');
+    var btn = document.getElementById('audioBtn');
+
+    console.log(btn);
+    if (audio !== null) {
+      if (audio.paused) {
+        audio.play();
+        btn.innerHTML = "Pause";
+      } else {
+        audio.pause();
+        btn.innerHTML = "Play";
+      }
+    } else {
+      console.error("Audio element not found!");
+    }
+  }
+
